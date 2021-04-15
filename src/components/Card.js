f33f7b82bb27c807271eb1a47218b70043e4f566
Dima Sheviakov <dima@MacBook-Pro-Dima.local>
2021-04-15 22:51:41 +0300

    feat: JS API for sprint 9

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,34 @@
 export default class Card {
-  constructor(selector, cardObject, handleCardClick) {
+  constructor({selector, cardObject, isDeletable, isLiked, handleCardClick, handleDeleteCard, handleLike}) {
     this._selector = selector;
-    this._name = cardObject.name;
-    this._link = cardObject.link;
+    this.cardObject = cardObject
+
+    this._isDeletable = isDeletable;
+    this.isLiked = isLiked;
+
     this._handleCardClick = handleCardClick;
+    this._handleDeleteCard = handleDeleteCard;
+    this._handleLike = handleLike;
   }
 
   generateCard() {
     this._element = this._getTemplate();
+    this._likeElement = this._element.querySelector('.card__like-btn');
+    this._likeCounterElement = this._element.querySelector('.card__like-counter');
+    this._deleteButtonElement = this._element.querySelector('.card__delete-btn');
+    this._imgElement = this._element.querySelector('.card__image');
+
     this._setEventListeners();
+    this._element.querySelector('.card__title').textContent = this.cardObject.name;
 
-    this._element.querySelector('.card__title').textContent = this._name;
+    if (this._isDeletable) {
+      this._deleteButtonElement.classList.add('card__delete-btn_enabled');
+    }
+    this.renderLikes();
 
-    const imgElement = this._element.querySelector('.card__image');
-    imgElement.src = this._link;
-    imgElement.alt = this._name;
+    this._imgElement.src = this.cardObject.link;
+    this._imgElement.alt = this.cardObject.name;
+
 
     return this._element;
   }
@@ -28,24 +42,23 @@ export default class Card {
   }
 
   _setEventListeners() {
-    this._element.querySelector('.card__like-btn')
-      .addEventListener('click', () => this._handleLike());
+    this._likeElement.addEventListener('click', this._handleLike);
 
-    this._element.querySelector('.card__delete-btn')
-      .addEventListener('click', () => this._handleDelete());
+    this._deleteButtonElement.addEventListener('click', this._handleDeleteCard);
 
-    this._element.querySelector('.card__image')
-      .addEventListener('click', evt => {
-        this._handleCardClick();
-      });
+    this._imgElement.addEventListener('click', this._handleCardClick);
   }
 
-  _handleLike() {
-    this._element.querySelector('.card__like-btn')
-      .classList.toggle('card__like-btn_active');
+  renderLikes() {
+    if (this.isLiked) {
+      this._likeElement.classList.add('card__like-btn_active');
+    } else {
+      this._likeElement.classList.remove('card__like-btn_active');
+    }
+    this._likeCounterElement.textContent = this.cardObject.likes.length;
   }
 
-  _handleDelete() {
+  delete() {
     this._element.closest('.card').remove();
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import './index.css';
 
+import Api from '../components/Api.js';
 import Card from '../components/Card.js';
 import Section from '../components/Section.js';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
-import {initialCards} from '../utils/initial-cards.js';
+
+function checkIsLiked(cardObject, userData) {
+  return cardObject.likes.some(like => like._id === userData._id);
+}
+
+function checkIsDeletable(cardObject, userData) {
+  return cardObject.owner._id === userData._id;
+}
 
 const validationConfig = {
   inputSelector: '.popup-form__input',
@@ -16,15 +24,54 @@ const validationConfig = {
   errorClass: 'popup-form__input-error_active'
 };
 
-
 const userInfo = new UserInfo({
   nameSelector: '.profile__fullname',
-  bioSelector: '.profile__bio'
+  bioSelector: '.profile__bio',
+  avatarSelector: '.profile__avatar-btn',
 });
+
+const cardsList = new Section('.cards', (cardItem) => cardsList.appendItem(createCard(cardItem)));
+
+const api = new Api({
+  cohortId: 'cohort-22',
+  token: '946aaeba-7448-46be-96a2-9a2018d54afe'
+})
+
+let userData; // global oobject for current user data
+
+api.getUserInfo()
+  .then(user => {
+    userData = user;
+    userInfo.setAvatar(userData);
+    userInfo.setUserInfo(userData);
+    return api.getInitialCards();
+  })
+  .then(cards => {
+    cards = cards.map(card => ({
+      isDeletable: checkIsDeletable(card, userData),
+      isLiked: checkIsLiked(card, userData),
+      cardObject: card,
+    }));
+    cardsList.renderItems(cards);
+  })
+  .catch(res => {
+    console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+  });
+
+
 const profilePopup = new PopupWithForm(
   '.popup_type_edit-profile',
-  ({fullname, bio}) => {
-    userInfo.setUserInfo({name: fullname, bio: bio});
+  ({fullname: name, bio: about}) => {
+    const newInfo = {name: name, about: about}
+    profilePopup.setPending(true);
+    api.setUserInfo(newInfo)
+      .then(newInfo => {
+        userInfo.setUserInfo(newInfo)
+        profilePopup.close();
+      })
+      .catch(res => {
+        console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+      })
   }
 );
 profilePopup.setEventListeners();
@@ -40,18 +87,71 @@ editProfileButton.addEventListener('click', () => {
   profilePopup.open();
 });
 
-function createCard(cardObject) {
-  const card = new Card(
-    '.card-template',
-    cardObject,
-    () => imagePopup.open(cardObject)
-  );
+
+const deleteCardPopup = new PopupWithForm (
+  '.popup_type_card-delete-confirmation',
+  () => {}
+);
+deleteCardPopup.setEventListeners();
+
+const imagePopup = new PopupWithImage('.popup_type_image-popup');
+imagePopup.setEventListeners();
+
+
+function createCard({cardObject, isDeletable, isLiked}) {
+  const card = new Card({
+    selector: '.card-template',
+    cardObject: cardObject,
+    isDeletable: isDeletable,
+    isLiked: isLiked,
+    handleCardClick: () => imagePopup.open(cardObject),
+    handleDeleteCard: () => {
+      deleteCardPopup.submitHandler = () => {
+        api.deleteCard(cardObject._id)
+          .then(() => {
+            card.delete();
+            deleteCardPopup.close();
+          })
+          .catch(res => {
+            console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+          });
+      }
+      deleteCardPopup.open();
+    },
+    handleLike: () => {
+      const method = (card.isLiked ? api.removeLike(cardObject._id) : api.addLike(cardObject._id));
+      method.then(cardObject => {
+        card.cardObject = cardObject;
+        card.isLiked = checkIsLiked(cardObject, userData);
+        card.renderLikes();
+      })
+      .catch(res => {
+        console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+      });
+    }
+  });
   return card.generateCard();
 }
 
+
 const createCardPopup = new PopupWithForm(
   '.popup_type_add-card',
-  ({placename, link}) => cardsList.addItem(createCard({name: placename, link: link}))
+  ({placename, link}) => {
+    const cardObject = {name: placename, link: link}
+    createCardPopup.setPending(true);
+    api.addNewCard(cardObject)
+      .then(cardObject => {
+        cardsList.addItem(createCard({
+          cardObject: cardObject,
+          isDeletable: true,
+          isLiked: false,
+        }))
+        createCardPopup.close();
+      })
+      .catch(res => {
+        console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+      })
+  }
 );
 createCardPopup.setEventListeners();
 
@@ -64,9 +164,20 @@ addCardButton.addEventListener('click', () => {
   createCardPopup.open()
 });
 
+
 const editAvatarPopup = new PopupWithForm(
   '.popup_type_edit-avatar',
-  ({avatar_url}) => {console.log(avatar_url)}
+  ({avatar_url}) => {
+    editAvatarPopup.setPending(true);
+    api.setAvatar(avatar_url)
+      .then(userData => {
+        userInfo.setAvatar(userData);
+        editAvatarPopup.close();
+      })
+      .catch(res => {
+        console.log(`Error '${res.status} ${res.statusText}' ${res.url}`);
+      })
+  }
 );
 editAvatarPopup.setEventListeners();
 
@@ -78,16 +189,3 @@ editAvatarButton.addEventListener('click', () => {
   editAvatarValidator.resetFormValidity();
   editAvatarPopup.open();
 });
-
-const imagePopup = new PopupWithImage('.popup_type_image-popup');
-imagePopup.setEventListeners();
-
-const cardsList = new Section(
-  {
-    items: initialCards,
-    renderer: (cardItem) => cardsList.addItem(createCard(cardItem))
-  },
-  '.cards'
-);
-
-cardsList.renderItems();
